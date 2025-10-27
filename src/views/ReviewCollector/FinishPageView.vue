06b998b843c7d092dcb3d222f4512b898ca4af5c
MashMinners <MannerheimLine@mail.ru>
2025-10-27 23:05:15 +1000

    #Финиш. Добавил таймер и переключение на вкладку с информацией

--- src/views/ReviewCollector/FinishPageView.vue
+++ src/views/ReviewCollector/FinishPageView.vue
@@ -14,6 +14,13 @@
   <div v-else>
     !
   </div>
+  <div class="flex-auto mt-1">
+    <prime-knob class="centered-text mt-1 mb-1" v-model="timerCount" :max="60"/>
+  </div>
+  <div class="flex-auto mt-3">
+    <prime-button severity="info" @click="goInfo">Информация</prime-button>
+  </div>
+
 </template>
 
 <script>
@@ -21,9 +28,29 @@ import {mapGetters} from "vuex";
 
 export default {
   name: "FinishPageView",
+  data() {
+    return {
+      timerEnabled: false,
+      timerCount: 60
+    }
+  },
   methods: {
-    checkReviewStatus(){
-
+    goInfo(){
+      this.$router.push('/info');
+    },
+    start(){
+      this.pauseTimer()
+      setTimeout(() => {
+        this.timerCount = 60;
+      }, 500);
+      //this.clearModel();
+      this.$router.push('/');
+    },
+    playTimer() {
+      this.timerEnabled = true;
+    },
+    pauseTimer() {
+      this.timerEnabled = false;
     }
   },
   computed: {
@@ -32,9 +59,31 @@ export default {
       getPacient: "app/getPacient"
     })
   },
-  //mounted(){
-    //this.checkReviewStatus();
-  //}
+  watch: {
+    //Синий таймер
+    timerEnabled(value) {
+      if (value) {
+        setTimeout(() => {
+          this.timerCount--;
+        }, 1000);
+      }
+    },
+    timerCount: {
+      handler(value) {
+        if (value > 0 && this.timerEnabled) {
+          setTimeout(() => {
+            this.timerCount--;
+          }, 1000);
+        }else if(value === 0){
+          this.start()
+        }
+      },
+      immediate: true // This ensures the watcher is triggered upon creation
+    },
+  },
+  mounted(){
+    this.playTimer()
+  }
 }
 </script>
 
