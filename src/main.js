96112d6669d46bd078930c9c0463ea50cb027081
MashMinners <Valehtelit@mail.ru>
2025-11-28 11:30:37 +1000

    #Main.js init

---
+++ src/main.js
@@ -0,0 +1,29 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+//import './registerServiceWorker' Надо понять, что это за сервис
+import PrimeVue from 'primevue/config';
+import PrimeVueModules from "@/configs/prime.vue.modules";
+import Aura from "@primevue/themes/aura";
+import store from "@/store";
+import router from './router'
+
+
+const app = createApp(App)
+//app.use(PrimeVue, PrimeVueModules.options[0]);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
+PrimeVueModules.modules.forEach(component => {
+    app.component('Prime'+component.name, component)
+})
+PrimeVueModules.directives.forEach(directive => {
+    app.directive(directive.name, directive.subject)
+})
+PrimeVueModules.services.forEach(service => {
+    app.use(service.name, service.subject)
+})
+app.use(store);
+app.use(router);
+app.mount('#app');
