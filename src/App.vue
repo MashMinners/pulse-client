af3cf5da887cf26c0682de4724783e7d6a32a9d0
MashMinners <Valehtelit@mail.ru>
2025-11-28 11:28:04 +1000

    #App.vue init

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="wrapper">
+    <component :is="layout">
+      <router-view />
+    </component>
+  </div>
+</template>
+
+<script>
+import ApplicationLayout from "@/layouts/ApplicationLayout.vue";
+import ControlPanelLayout from "@/layouts/ControlPanelLayout.vue";
+import 'primeflex/primeflex.css';
+export default {
+  components: {
+    ControlPanelLayout, ApplicationLayout
+  },
+  computed: {
+    layout () {
+      return (this.$route.meta.layout +'Layout')
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
