0e625966bf4949af088a18167e16e2590b76288f
MashMinners <Valehtelit@mail.ru>
2025-11-28 11:52:55 +1000

    #Layouts init

---
+++ src/layouts/ApplicationLayout.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app-header"></div>
+  <div id="app-content">
+    <!--suppress JSValidateTypes -->
+    <component
+        :is="getComponent"
+    />
+  </div>
+</template>
+<script>
+import {mapGetters} from "vuex";
+
+export default {
+  name: "ApplicationLayout",
+  data() {
+    return {
+
+    }
+  },
+  components: {
+
+  },
+  computed: {
+    getComponent(){
+      return (this.$route.matched[0].components.default)
+    },
+    ...mapGetters({
+    }),
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/layouts/DashboardLayout.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app-header"></div>
+  <div id="app-content"></div>
+  <div id="overlays"></div>
+</template>
+
+<script>
+
+export default {
+  name: "ControlPanelLayout",
+  data() {
+    return {
+      visible: false
+    }
+  },
+  computed: {
+    getComponent(){
+      return (this.$route.matched[0].components.default)
+    },
+  }
+}
+</script>
+
+<style>
+#app-content{
+  padding: 20px;
+  border: 1px solid #dee2e6;
+  min-height: calc(100vh - 110px)
+}
+</style>
